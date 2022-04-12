603d5a426e832a5f0d88e37f2260ac278506377f
Rory Magee <roryemagee@gmail.com>
2022-04-11 19:42:52 -0600

    Clean up default react files and add Header

---
+++ turing/3module/vapomodoros/vapomodoros/src/components/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './styles.css';
+import React, { Component} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ turing/3module/vapomodoros/vapomodoros/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+import './styles.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Test</h1>
+            <input type="text" placeholder="Search"></input>
+        </header>
+    )
+}
